77ae36896f84225c324f28ea9115a2abde94fc02
Miguel Osorio @Kelvur <miguel.osorio.next@bbva.com>
2023-09-24 22:15:16 +0200

    chore: recofigure eslint to not depend on typescript

--- .eslintrc.mjs
+++ .eslintrc.mjs
@@ -1,16 +1,12 @@
-module.exports = {
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-    ],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    root: true,
+export default {
     env: {
-        'browser': true,
-        'commonjs': true,
-        'es2017': true,
+        'es2021': true,
         'node': true,
+    },
+    extends: 'eslint:recommended',
+    parserOptions: {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module',
     },
     rules: {
         // Posible Errors
